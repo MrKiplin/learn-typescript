1ecbc6f7bde196b0169220536d950e534539b02b
Theo Jones <39280007+MrKiplin@users.noreply.github.com>
2019-06-17 23:59:38 +0100

    Added first task: task-001-fizzbuzz

---
+++ typescript-tasks/task-001-fizzbuzz.spec.ts
@@ -0,0 +1,7 @@
+describe("fizzBuzz", () => {
+  it("Should return FizzBuzz if value is multiple of 3 & 5", () => {});
+  it("Should return Fizz if value is multiple of 3", () => {});
+  it("Should return Buzz if value is multiple of 5", () => {});
+  it("Should return value if value is not a multiple of 3 or 5", () => {});
+  it("Should return an array containing FizzBuzz, Fizz, Buzz & values", () => {});
+});
---
+++ typescript-tasks/task-001-fizzbuzz.ts
@@ -0,0 +1,14 @@
+/*  _____ _         ____
+ * |  ___(_)_______| __ ) _   _ ________
+ * | |_  | |_  /_  /  _ \| | | |_  /_  /
+ * |  _| | |/ / / /| |_) | |_| |/ / / /
+ * |_|   |_/___/___|____/ \__,_/___/___|
+ * ======================================
+ * DESCRIPTION:
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5, 6 and 9.
+ *
+ * TASK:
+ * Find all multiples of 3 & 5 within a range of 1000. Return multiples of 3 as "Fizz", multiples of 5 as "Buzz" and multiples of both as "FizzBuzz".
+ */
+
+ 
