728b04893a2016bdea1c0357a0d9aa41fe71587a
Theo Jones <39280007+MrKiplin@users.noreply.github.com>
2019-06-19 00:47:42 +0100

    Added second task and updated README

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 A collection of tasks to solve using TypeScript and TDD (Test Driven Development). Test cases are included for each task.
 
+## Using the Repository
+
+This repository has been designed for intermediate developers who would like to become more familiar with TypeScript. Simply fork/clone to your local machine then navigate to the folder of your chosen task and run the tests `yarn test`. All tests will fail in their initial state. Your objective is to write code that makes all the tests pass.
+
 Happy hacking :)
 
 ## Getting Started
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+These instructions will get you a copy of the project up and running on your local machine.
 
 ### Prerequisites
 
@@ -42,10 +46,6 @@ Tests can be run globally from the root directory by running `yarn test`
 ```
 yarn test
 ```
-
-## Using the Repository
-
-Switch to the folder of your chosen task and run the tests `yarn test`. All tests will fail in their initial state. Your job is to write code that makes all the tests pass.
 
 ## Authors
 
--- typescript-tasks/task-001-fizzbuzz.spec.ts
+++ typescript-tasks/task-001-fizzbuzz.spec.ts
@@ -1,7 +1,27 @@
+import { fizzBuzz, fizzBuzzLogger } from "./task-001-fizzbuzz";
+
 describe("fizzBuzz", () => {
-  it("Should return FizzBuzz if value is multiple of 3 & 5", () => {});
-  it("Should return Fizz if value is multiple of 3", () => {});
-  it("Should return Buzz if value is multiple of 5", () => {});
-  it("Should return value if value is not a multiple of 3 or 5", () => {});
-  it("Should return an array containing FizzBuzz, Fizz, Buzz & values", () => {});
+  it("Should return FizzBuzz if value is multiple of 3 & 5", () => {
+    const result = fizzBuzz(0);
+    expect(result).toMatch("FizzBuzz");
+  });
+  it("Should return Fizz if value is multiple of 3", () => {
+    const result = fizzBuzz(3);
+    expect(result).toMatch("Fizz");
+  });
+  it("Should return Buzz if value is multiple of 5", () => {
+    const result = fizzBuzz(5);
+    expect(result).toMatch("Buzz");
+  });
+  it("Should return value if value is not a multiple of 3 or 5", () => {
+    const result = fizzBuzz(1);
+    expect(result).toEqual(1);
+  });
+  it("Should return an array containing FizzBuzz, Fizz, Buzz & values", () => {
+    const result = fizzBuzzLogger(5);
+    expect(result).toContain("FizzBuzz");
+    expect(result).toContain("Fizz");
+    expect(result).toContain("Buzz");
+    expect(result).toContain(1);
+  });
 });
