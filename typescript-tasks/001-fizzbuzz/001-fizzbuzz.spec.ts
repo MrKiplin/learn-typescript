e34abbc9b960f8ef407848b196803125cc35c8a3
MrKiplin <tjworks@hotmail.co.uk>
2020-04-12 17:35:06 +0100

    Updated test structure to match wiki and updated README

--- typescript-tasks/001-fizzbuzz/001-fizzbuzz.spec.ts
+++ typescript-tasks/001-fizzbuzz/001-fizzbuzz.spec.ts
@@ -1,10 +1,6 @@
 import { fizzBuzz, fizzBuzzLogger } from "./001-fizzbuzz";
 
 describe("fizzBuzz", () => {
-  it("Should return FizzBuzz if value is multiple of 3 & 5", () => {
-    const result = fizzBuzz(0);
-    expect(result).toMatch("FizzBuzz");
-  });
   it("Should return Fizz if value is multiple of 3", () => {
     const result = fizzBuzz(3);
     expect(result).toMatch("Fizz");
@@ -13,15 +9,19 @@ describe("fizzBuzz", () => {
     const result = fizzBuzz(5);
     expect(result).toMatch("Buzz");
   });
+  it("Should return FizzBuzz if value is multiple of 3 & 5", () => {
+    const result = fizzBuzz(0);
+    expect(result).toMatch("FizzBuzz");
+  });
   it("Should return value if value is not a multiple of 3 or 5", () => {
     const result = fizzBuzz(1);
     expect(result).toEqual(1);
   });
   it("Should return an array containing FizzBuzz, Fizz, Buzz & values", () => {
     const result = fizzBuzzLogger(5);
-    expect(result).toContain("FizzBuzz");
     expect(result).toContain("Fizz");
     expect(result).toContain("Buzz");
+    expect(result).toContain("FizzBuzz");
     expect(result).toContain(1);
   });
 });
--- typescript-tasks/001-fizzbuzz/001-fizzbuzz.ts
+++ typescript-tasks/001-fizzbuzz/001-fizzbuzz.ts
@@ -2,7 +2,7 @@
  * FizzBuzz
  *
  * DESCRIPTION:
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5, 6 and 9.
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
  *
  * TASK:
  * Find all multiples of 3 & 5 within a range of 1000. Return multiples of 3 as "Fizz", multiples of 5 as "Buzz" and multiples of both as "FizzBuzz".
